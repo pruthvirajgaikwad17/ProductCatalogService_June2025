e9af28d3ab7d3a750e46405a57c1cd4bb7cfe4b4
PruthvirajGaikwad <pruthvirajgaikwad1717@gmail.com>
2025-10-15 22:02:34 +0530

    AWS: Deploy code to aws by jar file

--- src/test/java/com/example/productcatalogservice_june2025/repos/ProductRepoTest.java
+++ src/test/java/com/example/productcatalogservice_june2025/repos/ProductRepoTest.java
@@ -1,5 +1,6 @@
 package com.example.productcatalogservice_june2025.repos;
 
+import com.example.productcatalogservice_june2025.models.Category;
 import com.example.productcatalogservice_june2025.models.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,34 @@ class ProductRepoTest {
     @Autowired
     private ProductRepo productRepo;
 
+
+
+
     @Test
+    public void addProductIntoRds() {
+        Product product1 = new Product();
+        product1.setId(1L);
+        product1.setName("Boomer");
+        product1.setPrice(10D);
+        Category category1 = new Category();
+        category1.setId(1L);
+        category1.setName("Bubble Gum");
+        product1.setCategory(category1);
+        productRepo.save(product1);
+
+
+        Product product2 = new Product();
+        product2.setId(10L);
+        product2.setName("IMac");
+        product2.setPrice(200000D);
+        Category category2 = new Category();
+        category2.setId(100L);
+        category2.setName("Electronics");
+        product2.setCategory(category2);
+        productRepo.save(product2);
+    }
+
+    //@Test
     public void testQueries() {
 //        List<Product> products = productRepo.getMeNameOfMyFavouriteProductById(2L);
 //        for(Product p: products){
