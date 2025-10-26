3eefdc4366fcc7b298c7d6fe9e471f309193da95
PruthvirajGaikwad <pruthvirajgaikwad1717@gmail.com>
2025-10-27 02:59:03 +0530

    redis implementation on the get product using id

---
+++ src/main/java/com/example/productcatalogservice_june2025/configs/AppConfig.java
@@ -0,0 +1,18 @@
+package com.example.productcatalogservice_june2025.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class AppConfig {
+
+    // we will create a bean for the service
+    @Bean
+    public RedisTemplate<String, Object> createRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    };
+}
--- src/main/java/com/example/productcatalogservice_june2025/models/BaseModel.java
+++ src/main/java/com/example/productcatalogservice_june2025/models/BaseModel.java
@@ -5,12 +5,15 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.Date;
 
+// Serializable means in Java is the process of converting an objects state into a byte stream
+// Deserialization is the reverse process, where the byte stream is used to reconstruct the original object in memory.
 @Getter
 @Setter
 @MappedSuperclass
-public class BaseModel {
+public class BaseModel implements Serializable {
     @Id
     private Long id;
 
